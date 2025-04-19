ba3dc21811c1d8208f408439d1a821f8ef1274c8
Joe Moses <joe@Joes-MacBook-Pro.local>
2025-04-19 01:35:32 +0100

    leaderboards

--- app/Livewire/Leaderboards.php
+++ app/Livewire/Leaderboards.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\UserStat;
+use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 
@@ -11,15 +12,42 @@ class Leaderboards extends Component
 
     public $stat;
 
+    public $table;
+
+    public $options = [
+        'goals',
+        'tackles',
+        'assists',
+        'saves',
+    ];
+
     public function mount(string $stat)
     {
-        $this->stat = $stat ?? 'goals';
+        $this->table = Cache::flexible($stat . '1', [600, 900], function () {
+            $this->stat = $stat ?? 'goals';
 
-        $stats = UserStat::where('stat', $this->stat)
-            ->where('value', '>', 0)
-            ->get('user_id', 'stat', 'value');
+            $calculatedStats = [];
 
-        dd($stats->count());
+            $stats = UserStat::where('stat', $this->stat)
+                ->where('value', '>', 0)
+                ->get(['user_id', 'stat', 'value'])
+                ->groupBy('user_id');
+
+            foreach ($stats as $group) {
+
+                $group->first()->load('user');
+
+                $total = [
+                    'sum' => $group->sum('value'),
+                    'user_name' => $group->first()->user->name,
+                    'user_id' => $group->first()->user->id,
+                ];
+
+                $calculatedStats[] = $total;
+            }
+
+            return collect($calculatedStats)->sortByDesc('sum');
+        });
     }
 
     public function render()
--- resources/views/livewire/leaderboards.blade.php
+++ resources/views/livewire/leaderboards.blade.php
@@ -1,3 +1,44 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="container mx-auto">
+
+        <div class="mb-5">
+            <h1 class="text-2xl font-bold">Leaderboards</h1>
+            <p class="text-gray-600">View the top players for {{ $stat }}.</p>
+            <div class="flex">
+                <span class="text-gray-600 pr-4">
+                    All stats:
+                </span>
+                @foreach($options as $option)
+                    <a href="{{ route('leaderboards', ['stat' => $option]) }}" class="btn btn-sm {{ $stat === $option ? 'btn-primary' : 'btn-secondary' }} mr-2">
+                        {{ $option }}
+                    </a> {!! $loop->last ? '' : '<span class="pr-3">/</span>' !!}
+                @endforeach
+            </div>
+        </div>
+
+        <x-card title="{{ $stat }} leaderboards">
+            <div class="overflow-x-auto">
+                <table class="table table-zebra w-full">
+                    <thead>
+                    <tr>
+                        <th class="text-left">Rank</th>
+                        <th class="text-left">Player</th>
+                        <th class="text-left">{{ $stat }}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($table as $index => $player)
+                        <tr class="listed">
+                            <td class="py-5">{{ $loop->iteration }}</td>
+                            <td class="py-5">
+                                <a href="{{ route('user.profile', $player['user_name']) }}" class="underline">{{ $player['user_name'] }}</a>
+                            </td>
+                            <td class="py-5">{{ $player['sum'] }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </x-card>
+    </div>
 </div>
